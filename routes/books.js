017a182e650ef18cbb4250e96c477f1830672ac3
Jamil Pinheiro <rafaeljamil@gmail.com>
2021-03-25 00:01:49 -0300

    Finished books routes and links

--- routes/books.js
+++ routes/books.js
@@ -65,15 +65,93 @@ router.post('/', async (req,res) => {
     }
 })
 
+//Rota que mostra o livro
+router.get('/:id', async (req,res) => {
+    try{
+        const book = await Book.findById(req.params.id).populate('author').exec()
+        res.render('books/show', {book: book})
+    }catch{
+        res.redirect('/')
+    }
+})
+
+//Rota que edita o livro
+router.get('/:id/edit', async (req,res) => {
+    try{
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    }catch{
+        res.redirect('/')
+    }
+})
+
+//Rota de atualizar livro
+router.put('/:id', async (req,res) => {
+    let book 
+    try{
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+        if(req.body.cover != null  && req.body.cover !== ''){
+            saveCover(book, req.body.cover)
+        }
+        await book.save()
+        res.redirect(`/books/${book.id}`)
+    }catch{
+        if(book != null){
+            renderEditPage(res, book, true)
+        }else{
+            res.redirect('/')
+        }
+        
+    }
+})
+
+//Rota pra apagar livros
+router.delete('/:id', async (req,res) => {
+    let book
+    try{
+        book = await Book.findById(req.params.id)
+        await book.remove()
+        res.redirect('/books')
+    }catch{
+        if(book != null){
+            res.render('books/show', {
+                book:book,
+                errorMessage: 'Impossível remover livro'
+            })  
+        }else{
+            res.redirect('/')
+        }
+    }
+})
+
 async function renderNewPage(res, book, hasError = false){
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false){
     try{
         const authors = await author.find({}) //passando o find com objeto vazio retorna todas as entradas do BD
         const params = {  //Parâmetros passados para a página renderizada
             authors: authors,
             book: book
         }
-        if (hasError) params.errorMessage = 'Erro ao criar livro.' //Se encontrar erro cria o parâmetro errorMessage
-        res.render('books/new', params) //Renderiza a página de novos livros passando todos os autores e o novo livro vazio
+        if (hasError){
+            if(form === 'edit'){
+                params.errorMessage = 'Erro ao atualizar livro.'
+            }else{
+                params.errorMessage = 'Erro ao criar livro.' //Se encontrar erro cria o parâmetro errorMessage
+            }
+        } 
+        res.render(`books/${form}`, params) //Renderiza a página de novos livros passando todos os autores e o novo livro vazio
     }catch{
         res.redirect('/books')
     }
